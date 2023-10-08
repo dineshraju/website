cd4d3c3849db911e5ad87cbfd0e77a8510f6cec9
Dinesh Raju <dineshraju@users.noreply.github.com>
2023-10-08 22:05:42 +0800

    add book highlights functionality

--- src/pages/notes/{markdownRemark.frontmatter__slug}.jsx
+++ src/pages/notes/{markdownRemark.frontmatter__slug}.jsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Link, graphql, Script } from 'gatsby'
 import "../../styles/simple.css"
+import config from '../../../gatsby-config'
 
 const Footer = (frontmatter) => {
   return (
@@ -10,6 +11,20 @@ const Footer = (frontmatter) => {
         <Link className="footercat" to={`/notes/`}>other notes</Link>
       </div>
   )
+}
+
+const generateAnchorLink = (text, anchors) => {
+  let link = text
+    .substring(0, 40)
+    .replaceAll(/<\/?\w+>/g, '')
+    .replaceAll(/[',’]/g, '')
+    .replaceAll(/\W/g, '-')
+    .toLowerCase()
+    .split('-').slice(0,3).join('-')
+
+    while (anchors[link]) { link = link + '-' }
+    anchors[link] = true
+    return link
 }
 
 const RegularTemplate = (frontmatter, html) => {
@@ -30,17 +45,7 @@ const RegularTemplate = (frontmatter, html) => {
   const anchors = {}
   const processedHtml = html.replaceAll(/<(h[34]|p)>/g, (match, p1, offset) => {
     const substart = offset + p1.length + 2
-    let anchorLink = html
-      .substring(substart, substart + 40)
-      .replaceAll(/<\/?\w+>/g, '')
-      .replaceAll(/[',’]/g, '')
-      .replaceAll(/\W/g, '-')
-      .toLowerCase()
-      .split('-').slice(0,3).join('-')
-    while (anchors[anchorLink]) { anchorLink = anchorLink + '-' }
-    anchors[anchorLink] = true
-
-    return `<${p1} id="${anchorLink}">`
+    return `<${p1} id="${generateAnchorLink(html.substring(substart, substart + 40), anchors)}">`
   })
 
   return (
@@ -49,6 +54,56 @@ const RegularTemplate = (frontmatter, html) => {
         dangerouslySetInnerHTML={{ __html: processedHtml }}
       />
       { renderThanks() }
+      { Footer(frontmatter) }
+    </div>
+  )
+}
+
+const BookTemplate = (frontmatter, html) => {
+
+  // TODO: find better way to do this
+  const cleanedHtml = html.replaceAll(/\n/gm, '').replaceAll(/<\/p><p>{/g, '{').replaceAll(/<\/p><p>/g, '<br />').replaceAll(/<\/p>/g, '')
+  const items = Array.from(cleanedHtml.matchAll(/{.+?}[^{]+/gm))
+  const anchors = {}
+
+  const hiddenClass = process.env.GATSBY_DEV ? '' : ' bookquotehidden'
+
+  return (
+    <div>
+      <h2 id="booktitle" className="bookfadein">{frontmatter.book}</h2>
+      <div className="bookheader">
+        { "(Quotes from a " }
+        <a href={frontmatter.url} target="_blank">book by {frontmatter.author}</a>
+        { `)` }
+      </div>
+      <div>
+      {
+        items.map((item, chidx) => {
+          const matches = item.toString().match('^{(.+)} (.+)$')
+          const chapter = matches[1]
+
+          const generateQuotes = () => matches[2].split('<br />').map((e, qidx) => {
+            const anc = generateAnchorLink(e, anchors)
+
+            return (
+              <div key={`q${qidx}`} id={anc} className={`bookquoterow${hiddenClass}`}>
+                <Link to={ `#${anc}`} className="bookanchor">#</Link>
+                <div className="bookquote">{e.split('<br>').map((s, qbidx) => (<p key={`q-${qidx}-${qbidx}`} >{s}</p>))}</div>
+              </div>
+            )
+          })
+
+          return (
+            <div key={`ch${chidx}`} className={`bookrow${hiddenClass}`}>
+              <div className={`bookchaptertitle`}>{chapter}</div>
+              <div className={`bookchapterrow`}>{generateQuotes()}</div>
+            </div>
+          )
+        })
+      }
+      </div>
+      <br />
+      <br />
       { Footer(frontmatter) }
     </div>
   )
@@ -111,22 +166,36 @@ const TranscriptTemplate = (frontmatter, html) => {
   )
 }
 
-const expandIPFS = hash => `https://${hash}.ipfs.cf-ipfs.com`
+const expandIPFS = hash => `https://cloudflare-ipfs.com/ipfs/${hash}`
 
 const BlogPostTemplate = ({ data }) => {
   const { frontmatter, html } = data.markdownRemark
-  let processedHtml = html.replaceAll(/ipfs:\/\/(baf[A-Za-z2-7]{56})/g, expandIPFS("$1"))
+  let processedHtml = html.replaceAll(/ipfs:\/\/(baf[A-Za-z2-7]{56})/g, expandIPFS('$1'))
 
-  // TODO: find better way to do this
-  setTimeout(() => {
-    const urlHash = typeof window !== "undefined" ? window.location.hash.substr(1) : null
+  React.useEffect(() => {
+    const urlHash = typeof window !== 'undefined' ? window.location.hash.substr(1) : null
     if (urlHash) {
       const elem = window.document.getElementById(urlHash)
-      if (elem) { elem.classList.add("highlight") }
+      if (frontmatter.book) {
+        if (elem) {
+          elem.parentElement.parentElement.classList.remove('bookquotehidden')
+          elem.classList.remove('bookquotehidden')
+          Array.from(window.document.getElementsByClassName('bookrow bookquotehidden')).forEach(e => e.remove())
+          Array.from(window.document.getElementsByClassName('bookquoterow bookquotehidden')).forEach(e => e.remove())
+        }
+      } else {
+        if (elem) { elem.classList.add('highlight') }
+      }
     }
-  }, 500)
+    if (frontmatter.book) {
+      const elem = window.document.getElementById('booktitle')
+      if (elem) { elem.classList.remove('bookfadein') }
+    }
+  })
 
   if (frontmatter.transcript) { return TranscriptTemplate(frontmatter, processedHtml) }
+
+  if (frontmatter.book) { return BookTemplate(frontmatter, processedHtml) }
 
   return RegularTemplate(frontmatter, processedHtml)
 }
@@ -140,6 +209,9 @@ export const pageQuery = graphql`
         updated(formatString: "Do MMM, YYYY")
         published(formatString: "Do MMM, YYYY")
         transcript
+        book
+        author
+        url
         thanks
         slug
         ogimage
@@ -151,8 +223,9 @@ export const pageQuery = graphql`
 export const Head = ({ data }) => {
   return (
     <>
-      <title>{ data.markdownRemark.htmlAst.children[0].children[0].value }</title>
+      <title>{ data.markdownRemark.frontmatter.book || data.markdownRemark.htmlAst.children[0].children[0].value }</title>
       <link rel='icon' type='image/png' sizes='32X32' href={ expandIPFS('bafkreicwsb5wa74xkn4hcy6rlvgct732jjydbl7rfeeav4jx5hfidklixa') } />
+      <link rel='canonical' href={ `${config.siteMetadata.siteUrl}notes/${data.markdownRemark.frontmatter.slug}/` } />
       <meta property='og:image' content= { expandIPFS(data.markdownRemark.frontmatter.ogimage || 'bafkreifbc5wnjsmcjoqg7ado3eaekv46kmis2rbuu6rzjiosxgtun3vqt4') } />
       <Script async src="https://www.googletagmanager.com/gtag/js?id=G-B9LVX0CBPY" />
       <Script>
